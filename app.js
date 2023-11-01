ab8ac70e166f97efb0a0cf599252ceab52c9e561
sujiina <66505782+sujiina@users.noreply.github.com>
2023-10-31 22:52:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas Connection URI (Replace with your own)
-const MONGO_URI = 'mongodb+srv://sujeena843:meet1974@cluster0.c4cbvx8.mongodb.net/Marketplace?retryWrites=true&w=majority';
+
+const MONGO_URI = 'mongodb+srv://sujeena843:Sujina@cluster0.c4cbvx8.mongodb.net/Marketplace?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
@@ -23,15 +22,15 @@ mongoose.connect(MONGO_URI, {
     console.error('Error connecting to MongoDB:', err);
   });
 
-// Use the productRoutes for product-related routes
+
 app.use('/api', productRoutes);
 
-// Add a route to handle the root URL ("/") and send the welcome message
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to DressStore application.' });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
